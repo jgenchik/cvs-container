8d60acdf78aa8fa717aa59a6c86d26d1a7749f6c
Joseph Genchik <jgenchik@gmail.com>
2021-12-14 14:48:46 -0500

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // encapsulation: ViewEncapsulation.ShadowDom,
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cvs-container';
+
+  showAngular: boolean =  JSON.parse(localStorage.getItem('showAngular') || 'true');  // converts string to boolean
+  showReact: boolean =  JSON.parse(localStorage.getItem('showReact') || 'true');
+  showStencil: boolean =  JSON.parse(localStorage.getItem('showStencil') || 'true');
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  ngOnInit(): void {
+      
+  }
+
+  showAngularChanged(event: MatSlideToggleChange) {
+    this.showAngular = event.checked;
+    localStorage.setItem('showAngular', event.checked.toString());
+  }
+
+  showReactChanged(event: MatSlideToggleChange) {
+    this.showReact = event.checked;
+    localStorage.setItem('showReact', event.checked.toString());
+  }
+
+  showStencilChanged(event: MatSlideToggleChange) {
+    this.showStencil = event.checked;
+    localStorage.setItem('showStencil', event.checked.toString());
+  }
+
+  angularButtonClicked(event: any) {
+    this.snackBar.open('Angular button clicked', '', {duration: 3000});
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { NotImplementedComponent } from './not-implemented/not-implemented.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotImplementedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
